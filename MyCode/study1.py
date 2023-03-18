feb4be3abf328442c37d817dca3c6de921b96017
Siterow <823978463@qq.com>
2023-03-18 16:04:19 +0800

    字符串学习

--- MyCode/study1.py
+++ MyCode/study1.py
@@ -82,8 +82,93 @@
 #         print(num)
 
 """公鸡5元一只，母鸡3元一只，小鸡1元三只，用100块钱买一百只鸡，问公鸡、母鸡、小鸡各有多少只？"""
-for i in range(0, 20):
-    for j in range(0, 33):
-        k = 100 - i - j
-        if i * 5 + j * 3 + k / 3 == 100:
-            print(f'公鸡{i},母鸡{j},小鸡{k}')
+# for i in range(0, 20):
+#     for j in range(0, 33):
+#         k = 100 - i - j
+#         if i * 5 + j * 3 + k / 3 == 100:
+#             print(f'公鸡{i},母鸡{j},小鸡{k}')
+
+"""赌博游戏"""
+
+# money = 1000
+# while money > 0:
+#     need_go_on = False
+#     print('你的总资产为:', money)
+#     while True:
+#         debt = int(input('押注'))
+#         if 0 < debt <= money:
+#             break
+#     firstNum = random.randint(1, 6) + random.randint(1, 6)
+#     print(f'第1次点数{firstNum}')
+#     if firstNum == 7 or firstNum == 11:
+#         print(f'{firstNum}点，玩家胜')
+#         money += debt
+#     elif firstNum == 2 or firstNum == 3 or firstNum == 12:
+#         print(f'{firstNum}点，庄家胜')
+#         money -= debt
+#     else:
+#         need_go_on = True
+#         n = 1
+#         while need_go_on:
+#             n += 1
+#             need_go_on = False
+#             Num = random.randint(1, 6) + random.randint(1, 6)
+#             print(f'第{n}次点数{Num}')
+#             if Num == 7:
+#                 print(f'{Num}点,庄家胜')
+#                 money -= debt
+#             elif Num == firstNum:
+#                 print(f'{Num}点,玩家胜')
+#                 money += debt
+#             else:
+#                 need_go_on = True
+# print('破产')
+# import flag
+# import random
+# import math
+
+
+"""WYS发我的竞赛题"""
+# flag = '02}8c ms13'
+# length = len(flag)
+# print(length)
+# temp = [i for i in range(length)]
+# result = ''
+# for j in range(0, 15):
+#     for i in range(1, length):
+#         temp[(2 * i) % length] = flag[i]
+#     flag = temp[:]
+# print(temp)
+# out = ''
+# for i in range(len(temp)):
+#     out += str(temp[i])
+# print(out[:10])
+# b = random.randint(0, length)
+# while True:
+#     a = random.randint(0, length)
+#     if math.gcd(a, length) == 1:
+#         break
+# print(a, b)
+# cipher = []
+# for i in out:
+#     c = (ord(i) * a + b)
+#     cipher.append(c)
+# print(cipher)
+
+
+def foo():
+    pass
+
+
+def bar():
+    pass
+
+
+# __name__是Python中一个隐含的变量它代表了模块的名字
+# 只有被Python解释器直接执行的模块的名字才是__main__
+if __name__ == '__main__':
+    print('call foo()')
+    foo()
+    print('call bar()')
+    bar()
+
